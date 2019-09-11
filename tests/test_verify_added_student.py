02317463ba03f3344652c7a9395e9573f3e8629c
Quyen Doan <qdoan1651@gmail.com>
2019-09-10 23:21:28 -0800

    Refactored code to read student id and expected data from file instead

--- tests/test_verify_added_student.py
+++ tests/test_verify_added_student.py
@@ -1,19 +1,18 @@
 import requests, json
 
 def test_verify_added_user():
-    id = '144865'
-    api_url = api_url = 'http://thetestingworldapi.com/api/studentsDetails/{}'.format(id)
+    # Getting student info from file
+    with open('../resources/student_info.json', 'r') as infile:
+        expected_data = json.load(infile)
 
-    request_data = {
-        "first_name": "Jason",
-        "middle_name": "API",
-        "last_name": "Path",
-        "date_of_birth": "5-5-1955"
-    }
+    # Getting student id from file
+    with open('../resources/student_id.txt', 'r') as infile:
+        student_id = infile.read()
+    api_url = api_url = 'http://thetestingworldapi.com/api/studentsDetails/{}'.format(student_id)
 
     response = requests.get(api_url)
     assert response.status_code == 200
-    # print(response.text)
+
     response_json = json.loads(response.text)
     status = response_json['status']
     response_id = response_json['data']['id']
@@ -23,8 +22,14 @@ def test_verify_added_user():
     dob = response_json['data']['date_of_birth']
 
     assert status == "true"
-    assert str(response_id) == id
-    assert first_name == request_data['first_name']
-    assert middle_name == request_data['middle_name']
-    assert last_name == request_data['last_name']
-    assert dob == request_data['date_of_birth']
+    assert str(response_id) == student_id
+    assert first_name == expected_data['first_name']
+    assert middle_name == expected_data['middle_name']
+    assert last_name == expected_data['last_name']
+    assert dob == expected_data['date_of_birth']
+
+# if __name__ == '__main__':
+#     filename = '../resources/student_id.txt'
+#     with open(filename, 'r') as infile:
+#         student_id = infile.read()
+#     print(student_id)
