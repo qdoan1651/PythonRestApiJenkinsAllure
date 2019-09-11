5ca8e2d9a31ca87f2963025f70c7f7ca6773b47a
Quyen Doan <qdoan1651@gmail.com>
2019-09-10 23:47:44 -0800

    Added delete student test

---
+++ tests/test_delete_student.py
@@ -0,0 +1,18 @@
+import requests, json
+
+def test_delete_student():
+    # Getting student id from file
+    with open('../resources/student_id.txt', 'r') as infile:
+        student_id = infile.read()
+    api_url = api_url = 'http://thetestingworldapi.com/api/studentsDetails/{}'.format(student_id)
+
+    response = requests.delete(api_url)
+    assert response.status_code == 200
+
+    response_json = json.loads(response.text)
+
+    status = response_json['status']
+    msg = response_json['msg']
+
+    assert status == "true"
+    assert msg == 'Delete  data success'
