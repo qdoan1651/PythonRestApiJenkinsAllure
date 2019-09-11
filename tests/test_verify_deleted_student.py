0895758a8dce52c822e81e03082ccc5e7217d12b
Quyen Doan <qdoan1651@gmail.com>
2019-09-11 00:03:14 -0800

    Add test to verify deleted student

---
+++ tests/test_verify_deleted_student.py
@@ -0,0 +1,18 @@
+import requests, json
+
+def test_updated_student_info():
+    # Getting student id from file
+    with open('../resources/student_id.txt', 'r') as infile:
+        student_id = infile.read()
+    api_url = api_url = 'http://thetestingworldapi.com/api/studentsDetails/{}'.format(student_id)
+
+    response = requests.get(api_url)
+    assert response.status_code == 200
+
+    response_json = json.loads(response.text)
+    print(response_json)
+    status = response_json['status']
+    msg = response_json['msg']
+
+    assert status == "false"
+    assert msg == 'No data Found'
